6cfd6c9c0da9adee5afe4d39e455de323937a8e3
Srinath <srinath9589@gmail.com>
2024-02-03 12:46:22 +0530

    initial commit

---
+++ src/main/java/com/hpa/UserServiceHpaApplication.java
@@ -0,0 +1,13 @@
+package com.hpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceHpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceHpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hpa/UserServiceHpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceHpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
